94fae90046bd57e691e84ffa357e63872df42af9
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-11-06 21:02:32 +0600

    problem 3 solve done

---
+++ src/problem-3.ts
@@ -0,0 +1,26 @@
+// Write a TypeScript function countWordOccurrences that takes a sentence (string) and a word (string). It should return the number of times the word appears in the sentence, case-insensitively.
+
+// // Sample Input:
+// countWordOccurrences("TypeScript is great. I love TypeScript!", "typescript");
+
+// // Sample Output:
+// 2;
+
+{
+    function countWordOccurrences(sentence: string, word: string): number {
+        const lowercaseWord = word.toLowerCase() //  word value is converted to lowercase
+        // First I convert the sentence to lowercase then I split it with spaces
+        const splitSentence = sentence.toLowerCase().split(' ') 
+        //  then i compare with split CSSMathValue,if word includes in sentence this value will be return 
+        const filterUniqueWord = splitSentence.filter(singleWord => singleWord.includes(lowercaseWord))
+
+        return filterUniqueWord.length
+
+    }
+
+
+    console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "Typescript"));
+
+
+
+}
