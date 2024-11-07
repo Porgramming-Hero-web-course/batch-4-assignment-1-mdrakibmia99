931f492d562c542d0eebed9a3a5e0b35fa34e8ff
mdrakibmia99 <rkrakibhasan680@gmail.com>
2024-11-07 15:15:27 +0600

    modify 3 no problem,

--- src/problem-3.ts
+++ src/problem-3.ts
@@ -7,19 +7,35 @@
 // 2;
 
 {
+
+    // update code
     function countWordOccurrences(sentence: string, word: string): number {
         const lowercaseWord = word.toLowerCase() //  word value is converted to lowercase
         // First I convert the sentence to lowercase then I split it with spaces
         const splitSentence = sentence.toLowerCase().split(' ') 
         //  then i compare with split CSSMathValue,if word includes in sentence this value will be return 
-        const filterUniqueWord = splitSentence.filter(singleWord => singleWord.includes(lowercaseWord))
+        const filterUniqueWord = splitSentence.filter(singleWord => singleWord === lowercaseWord)
 
         return filterUniqueWord.length
 
     }
 
-
     console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "Typescript"));
+    
+    // previous code
+    // function countWordOccurrences(sentence: string, word: string): number {
+    //     const lowercaseWord = word.toLowerCase() //  word value is converted to lowercase
+    //     // First I convert the sentence to lowercase then I split it with spaces
+    //     const splitSentence = sentence.toLowerCase().split(' ') 
+    //     //  then i compare with split CSSMathValue,if word includes in sentence this value will be return 
+    //     const filterUniqueWord = splitSentence.filter(singleWord => singleWord.includes(lowercaseWord))
+
+    //     return filterUniqueWord.length
+
+    // }
+
+
+    // console.log(countWordOccurrences("TypeScript is great. I love TypeScript!", "Typescript"));
 
 
 
